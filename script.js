3d73771d65938ded240f295e55bf61c06839d1a6
Jake Caddy <Jake.Caddy@infinigate-cloud.com>
2024-05-30 13:09:14 +0100

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('registerBtn').addEventListener('click', function() {
-    // Replace the URL below with the desired redirection URL
-    window.location.href = 'https://example.com/register';
-});
